bfb75ce2338f15fb028736d3b2c2260cc7495892
Shaun <shaun.lobsinger@gmail.com>
2019-08-04 19:42:36 -0700

    Create PhysicsObject.cs

---
+++ movement/2D/Platformer/PhysicsObject.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+    public float minGroundNormalY = .65f;
+
+    protected Vector2 targetVelocity;
+    protected bool grounded;
+    protected Vector2 velocity;
+    protected Vector2 groundNormal;
+    protected Rigidbody2D rb2d;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        contactFilter.useTriggers = false;
+
+        // this uses the default settings within Unity to determine what we should collide with.
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+
+        contactFilter.useLayerMask = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = Vector2.zero;
+        ComputeVelocity();
+    }
+
+    protected virtual void ComputeVelocity()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        // calculate the velocity of the object
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
+
+        grounded = false;
+
+
+        // lets get the chnage in position
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        // this is handle slopes...
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        Movement(move, false);
+
+        // lets get horizontal movement
+        move = Vector2.up * deltaPosition.y;
+
+        Movement(move, true);
+    }
+
+    void Movement(Vector2 move, bool yMovement)
+    {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance)
+        {
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+
+            hitBufferList.Clear();
+
+            // lets add all the hits to the list
+            for(int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            // lets now evaluate all the hits 
+            for(int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    grounded = true;
+
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+       
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+}
